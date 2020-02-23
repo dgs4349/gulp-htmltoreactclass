88bf8357ed9ebeeeb4238de7cfe3c837ab8a1af5
dgs4349 <dgs4349@rit.edu>
2020-02-23 15:53:55 -0500

    Added better unknown error log, added copy task

--- gulp-htmltoreact/test/gulpfile.js
+++ gulp-htmltoreact/test/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const del = require("del");
 const logger = require("node-color-log")
+const rename = require("gulp-rename");
 
 const contentequals = require("gulp-contentequals");
 
@@ -60,13 +61,44 @@ testFailCases = [
     (done)=>testFailFile("./fail-lowercasename.jsx", "Found the issue! Class name is lowercase!!", done),
     (done)=>testFailFile("./fail-missingcontent.jsx", "Found the issue! ReactClass is missing the html content!!", done),
     (done)=>testFailFile("./fail-missingreactclass.jsx", "Found the issue! Missing the ReactClass!!", done),
+
+    // report unknown error
     (done)=>{
         if(!success) {
             if(currentResults < failCases) logFail(failCaseMatchMessage);
-            else logFail("Unknown issue, compare file to success case: 'test/success.jsx'.");
+
+            // very verbose, but let's be honest we'll need these instructions if I modify this tool in the future
+            else logFail(`
+                Test failed! Test file contents do not match the contents of the specified success result!
+                Check the differences between the test file 'test/test-dest/example.jsx' with it's expected success result: 'test/success.jsx'.
+
+                If new desired behavior has been added and the test should have succeeded, copy the new success result:
+                    - navigate to the bottom of test/gulpfile.js
+                    - copyFrom var should be './test-dest/example.jsx', navigated from within directory test/
+                    - change value of copyToName to desired filename, should be'success.jsx'
+                    - change copyToDirectory to desired target directory, should remain in test directory: '.'
+                    
+                If this is a common error, error case can be added using the same method, and then adding the test case function to the testFailCases array
+            `);
         }
         done();
     },
 ];
 
 gulp.task("test", gulp.series(clean, transform, compare, ...testFailCases));
+
+
+
+// ****************************************************************************** //
+// ADJUST VARS HERE TO COPY FILES EXACTLY //
+const copyFrom = "./test-dest/example.jsx";
+const copyToName = "copiedexample.jsx";
+const copyToDirectory = ".";
+
+function copy(done) {
+    gulp.src(copyFrom)
+        .pipe(rename(copyToName))
+        .pipe(gulp.dest(copyToDirectory));
+    done();
+}
+gulp.task("copy", copy);
